11cced20f8181177a740c621eea421b3b1d75cec
Salvatore Allegra <salallegra@gmail.com>
2026-07-24 18:10:24 -0700

    Move spacecraft data to object

--- Aeugis.TelemetrySimulator/Program.cs
+++ Aeugis.TelemetrySimulator/Program.cs
@@ -1,10 +1,22 @@
-﻿string spacecraftName = "Odyssey-1";
-double temperature = 82.4;
-double batteryVoltage = 21.7;
-bool antennaOnline = true;
+﻿using Aeugis.TelemetrySimulator;
 
+Spacecraft odyssey = new Spacecraft
+{
+    Name = "Odyssey-1",
+    Temperature = 82.4,
+    BatteryVoltage = 21.7,
+    AntennaOnline = true
+};
+
+Spacecraft voyager = new Spacecraft
+{
+    Name = "Voyager-7",
+    Temperature = 76.8,
+    BatteryVoltage = 22.1,
+    AntennaOnline = false
+};
 Console.WriteLine("AEGIS MISSION CONTROL");
-Console.WriteLine("Spacecraft: " + spacecraftName);
-Console.WriteLine("Temperature: " + temperature);
-Console.WriteLine("Battery Voltage: " + batteryVoltage);
-Console.WriteLine("Antenna Online: " + antennaOnline);
+Console.WriteLine("Spacecraft: " + odyssey.Name);
+Console.WriteLine("Temperature: " + odyssey.Temperature);
+Console.WriteLine("Battery Voltage: " + odyssey.BatteryVoltage);
+Console.WriteLine("Antenna Online: " + odyssey.AntennaOnline);
---
+++ Aeugis.TelemetrySimulator/Spacecraft.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Aeugis.TelemetrySimulator
+{
+    public class Spacecraft
+    {
+        public string Name { get; set; } = "";
+
+        public double Temperature { get; set; }
+
+        public double BatteryVoltage { get; set; }
+
+        public bool AntennaOnline { get; set; }
+    }
+}
