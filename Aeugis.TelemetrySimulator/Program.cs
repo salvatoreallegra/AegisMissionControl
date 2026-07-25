a26b9a5fc46d2a19e1e65b94a74f4b66077ad22d
Salvatore Allegra <salallegra@gmail.com>
2026-07-24 17:50:58 -0700

    Print Odyssey-1 Telemetry data

--- Aeugis.TelemetrySimulator/Program.cs
+++ Aeugis.TelemetrySimulator/Program.cs
@@ -1,1 +1,10 @@
-﻿Console.WriteLine("Hello, World!");
+﻿string spacecraftName = "Odyssey-1";
+double temperature = 82.4;
+double batteryVoltage = 21.7;
+bool antennaOnline = true;
+
+Console.WriteLine("AEGIS MISSION CONTROL");
+Console.WriteLine("Spacecraft: " + spacecraftName);
+Console.WriteLine("Temperature: " + temperature);
+Console.WriteLine("Battery Voltage: " + batteryVoltage);
+Console.WriteLine("Antenna Online: " + antennaOnline);
